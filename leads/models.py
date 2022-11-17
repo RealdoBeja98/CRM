fa78a7b891b289329f86f042fa8626f432ba908a
RealdoBeja98 <realdo.beja@mail.polimi.it>
2022-11-17 21:56:12 +0100

    render template

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+
+class Lead(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True) #models.CASCADE
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+
