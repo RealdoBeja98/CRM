874f77bc5d2174fe4409f735f8d9e6a031b87ca8
RealdoBeja98 <realdo.beja@mail.polimi.it>
2022-11-21 21:12:34 +0100

    lead_dettail

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,9 @@
+
+from django.urls import path, include
+
 from django.contrib import admin
-from django.urls import path
-from leads.views import home_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_page)
+    path('admin', admin.site.urls),
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
 
-def home_page(request):
+def lead_list(request):
     #return HttpResponse("Hello world")
-    return render()
+    leads = Lead.objects.all()
+    context = {
+        "leads" : leads
+    }
+    return render(request, "lead_list.html", context) 
 
+def lead_dettail(request, pk): #pk is primary key
+    lead = Lead.objects.get(id = pk)
+    context = {
+        "lead" : lead
+    }
+    return render(request, "lead_dettail.html", context )
