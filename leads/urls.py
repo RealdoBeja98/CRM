053a2963e87bd47571869a7d8f17f312d142bb28
RealdoBeja98 <realdo.beja@mail.polimi.it>
2022-11-22 20:12:54 +0100

    lead_dettail

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from .views import lead_list, lead_dettail 
+from .views import lead_list, lead_dettail, lead_create 
 
 app_name = "leads"
 
 urlpatterns = [
     path('', lead_list),
-    path('<pk>', lead_dettail)
+    path('<int:pk>/', lead_dettail),
+    path('create/', lead_create)
+
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 # Create your views here.
 
@@ -18,3 +19,25 @@ def lead_dettail(request, pk): #pk is primary key
         "lead" : lead
     }
     return render(request, "lead_dettail.html", context )
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                firstName = first_name,
+                lastName = last_name,
+                age = age,
+                agent = agent
+            )
+            print("Lead created")
+            return redirect ("/leads/")
+    context = {
+        "form" : form
+    }
+    return render(request, "lead_create.html", context)
